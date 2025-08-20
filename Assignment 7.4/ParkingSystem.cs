df8d076709fd4c5e9418283b1533ca68284b29a1
ElijahA1 <elijah.abourezk@outlook.com>
2025-08-20 11:54:09 -0700

    Add project files.

---
+++ Assignment 7.4/ParkingSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7._4
+{
+    internal class ParkingSystem
+    {
+        private uint AvailableBigCarSlots;
+        private uint AvailableMediumCarSlots;
+        private uint AvailableSmallCarSlots;
+
+        public ParkingSystem(uint bigCarSlots, uint mediumCarSlots, uint smallCarSlots)
+        {
+            AvailableBigCarSlots = bigCarSlots;
+            AvailableMediumCarSlots = mediumCarSlots;
+            AvailableSmallCarSlots = smallCarSlots;
+        }
+
+        public bool AddCar(uint car)
+        {
+            if(AvailableBigCarSlots == 0 && car == 1)
+                return false;
+            if (AvailableMediumCarSlots == 0 && car == 2)
+                return false;
+            if (AvailableSmallCarSlots == 0 && car == 3)
+                return false;
+            switch (car)
+            {
+                case 1:
+                    AvailableBigCarSlots--;
+                    return true;
+                case 2:
+                    AvailableMediumCarSlots--;
+                    return true;
+                case 3:
+                    AvailableSmallCarSlots--;
+                    return true;
+                default:
+                    Console.Error.WriteLine("Not an available car type");
+                    return false;
+            }
+        }
+    }
+}
---
+++ Assignment 7.4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment_7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParkingSystem parkingSystem = new(1, 1, 0);
+
+            Console.WriteLine(parkingSystem.AddCar(1));//true
+            Console.WriteLine(parkingSystem.AddCar(2));//true
+            Console.WriteLine(parkingSystem.AddCar(3));//false
+            Console.WriteLine(parkingSystem.AddCar(1));//false
+
+            Console.ReadKey();
+        }
+    }
+}
